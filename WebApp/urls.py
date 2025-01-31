d9ab495218bb3364ab532cb375c47b3cf107638e
Anay0305 <anayg0305@gmail.com>
2025-02-01 04:30:20 +0530

    s

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -18,14 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse,render
 
 def fun(requests):
-    return HttpResponse("""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Testing</title>
-</head>""")
+    return render("home")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
