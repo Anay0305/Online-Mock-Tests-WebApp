575384970564879d56b58cf7ada946d6a7186456
Anay0305 <anayg0305@gmail.com>
2025-02-01 21:14:04 +0530

    s

--- Tests/views.py
+++ Tests/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Attempt
 import json
